a4b4b9b9619cdacb437e23831ee5ac173fd05169
Sulaiman3352 <32817650+Sulaiman3352@users.noreply.github.com>
2023-04-03 10:01:56 +0300

    Add files via upload

--- 4th lesson/ex.py
+++ 4th lesson/ex.py
@@ -1,3 +1,14 @@
 n = int(input("How may numbers in list? "))
 print("input", n, "numbers in single line with spaces")
+a = [int(s) for s in input().split()]
+
+max_a = max(a)
+max_2 = 0
+
+for i in range(0, n):
+    if a[i] > max_2:
+        if a[i] < max_a:
+            max_2=a[i]
+
+print(max_a, max_2)
 
